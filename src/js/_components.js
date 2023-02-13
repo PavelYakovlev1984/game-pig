8199bbbcbcfa49329637db82166a4207f3825367
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-02-14 00:11:36 +0300

    start 2

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,1 +1,11 @@
-console.log('components');
+'use strict';
+console.log('Pavel Yakovlev');
+
+let score0Element = document.querySelector('#score--0');
+let score1Element = document.querySelector('#score--1');
+let diceElement = document.querySelector('.dice');
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
+
