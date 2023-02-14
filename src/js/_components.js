32ad4ffd339dc8e6fac6ed3332bfe8fbeb37da97
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-02-14 20:29:32 +0300

    Update _components.js

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,5 +1,5 @@
 'use strict';
-console.log('Pavel Yakovlev');
+// console.log('Pavel Yakovlev');
 
 let score0Element = document.querySelector('#score--0');
 let score1Element = document.querySelector('#score--1');
@@ -9,3 +9,17 @@ score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden');
 
+let btnNewElement = document.querySelector('.btn--new');
+let btnRollElement = document.querySelector('.btn--roll');
+let btnHoldElement = document.querySelector('.btn--hold');
+let currentScore = Number(document.getElementById('current--0').textContent);
+
+btnRollElement.addEventListener('click', function(){
+  let diceNumber = Math.trunc(Math.random() * 6) + 1;
+  if(diceNumber !== 1) {
+  currentScore = currentScore + diceNumber;
+  document.getElementById('current--0').textContent = currentScore;
+  }else if (diceNumber == 1) {
+    document.getElementById('current--0').textContent = 0;
+  }
+})
